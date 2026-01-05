6f03afd8276f7c164b879c226106dfdcbb4e7329
Lollosmino42 <arc042r@proton.me>
2026-01-05 20:26:03 +0100

    Fixed 'range' and 'generator' macros

--- pointers.h
+++ pointers.h
@@ -10,21 +10,27 @@
 
 
 #if __DEVKIT_USE_CUSTOM_ALLOCATOR
+#define devkit_asiterable __devkit_asiterable
+#define devkit_linspace __devkit_linspace
+#define devkit_generator __devkit_generator
+#define devkit_range __devkit_range
+
+#else
+#define devkit_asiterable( array, length, typesize) __devkit_asiterable( nullptr, (array), (length), typesize)	
+#define devkit_linspace( start, end, steps) __devkit_linspace( nullptr, (start), (end), (steps))
+#define devkit_generator( base, length, typesize, map) __devkit_generator( nullptr, (base), (length), (typesize), (map))
+#define devkit_range( start, end) __devkit_range( nullptr, (start), (end))
+
+#endif
+
 #define asiterable devkit_asiterable
 #define linspace devkit_linspace
 #define generator devkit_generator
 #define range devkit_range
 
-#else
-#define asiterable( array, length, typesize) devkit_asiterable( nullptr, (array), (length), typesize)	
-#define linspace( start, end, steps) devkit_linspace( nullptr, (start), (end), (steps))
-#define generator( base, length, typesize, map) devkit_range( nullptr, (base), (length), (typesize), (map))
-#define range( start, end) devkit_generator( nullptr, (start), (end))
 
-#endif
-
-
-#define contains( array, len, var) devkit_contains( array, len, sizeof(array[0]), &(var))
+#define devkit_contains( array, len, var) __devkit_contains( (array), (len), sizeof(array[0]), &(var))
+#define contains devkit_contains
 
 /* Unreferences to pointer after casting */
 #define unref( type) *(type*)
@@ -33,7 +39,7 @@
 #define refof( type, ...) (type[]) {__VA_ARGS__}
 
 /* Asserts that ptr is not null and returns it */
-#define nonnull( ptr) ( assert( (ptr) != nullptr), ptr)
+#define nonnull( ptr) ( assert( (ptr) != nullptr), (ptr))
 
 
 
@@ -41,7 +47,7 @@
 /* IMPLEMENTATION */	
 
 /* Returns true if 'array' contains 'value' */
-extern bool devkit_contains( 
+extern bool __devkit_contains( 
 		const void *const array, 
 		const size_t len, 
 		const size_t typesize, 
@@ -56,7 +62,7 @@ extern bool devkit_contains(
 
 
 /* Creates an iterable object associated with the 'array' of 'length' items of 'typesize' */
-extern inline Iterable devkit_asiterable( DEVKIT_ALLOCATOR *alloc, void* array, size_t length, size_t typesize) {
+extern inline Iterable __devkit_asiterable( DEVKIT_ALLOCATOR *alloc, void* array, size_t length, size_t typesize) {
 	return (Iterable) { 
 		.alloc=alloc,
 		.typesize=typesize, 
@@ -67,15 +73,15 @@ extern inline Iterable devkit_asiterable( DEVKIT_ALLOCATOR *alloc, void* array, 
 
 
 /* Gives a set of numbers from 'start' to 'end' - 1 */
-extern long* devkit_range( DEVKIT_ALLOCATOR *alloc, unsigned long start, unsigned long end) {
-	long *items = DEVKIT_MALLOC( alloc, (end - start) * sizeof(long));
+extern long* __devkit_range( DEVKIT_ALLOCATOR *alloc, unsigned long start, unsigned long end) {
+	long *items = DEVKIT_CALLOC( alloc, end - start, sizeof(long));
 	for (size_t x = start, idx = 0; x < end; x++, idx++) items[idx] = x;
 	return items;
 }
 
 
 /* Generates a new array from 'base' with 'length' elements of 'typesize' with 'map' */
-extern void* devkit_generator( DEVKIT_ALLOCATOR *alloc, void *base, size_t length, size_t typesize, Map map) {
+extern void* __devkit_generator( DEVKIT_ALLOCATOR *alloc, void *base, size_t length, size_t typesize, Map map) {
 	void *items = DEVKIT_CALLOC( alloc, length, typesize);
 	memcpy( items, nonnull(base), length*typesize);
 	for (size_t idx = 0; idx < length; idx++) {
@@ -86,7 +92,7 @@ extern void* devkit_generator( DEVKIT_ALLOCATOR *alloc, void *base, size_t lengt
 
 
 /* Returns a uniform devkit_range of 'steps' values between 'start' and 'end' */
-extern long double* devkit_linspace( DEVKIT_ALLOCATOR *alloc, const double start, const double end, const unsigned steps) {
+extern long double* __devkit_linspace( DEVKIT_ALLOCATOR *alloc, const double start, const double end, const unsigned steps) {
 	assert( steps >= 2);
 
 	long double delta = (end - start) / (steps - 1);
